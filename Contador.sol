e887c9ce9eef97637bc1c3e57b4f82ec62dc1731
Tomatitto Chorry <68964165+TomatittoChorry@users.noreply.github.com>
2021-07-29 20:38:13 -0300

    solidity

---
+++ Contador.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.17;
+
+contract Contador { 
+    uint256 count;
+    function Contador(uint256 _count) public {
+        count = _count;
+        
+    }
+    
+    function setCount(uint256 _count) public {
+        count = _count;
+        
+    }
+    
+    function incrementCount() public {
+        count +=1;
+    }
+    
+    function getCount() public view returns(uint256) {
+        return count;
+    }
+    
+    function getNumber() public pure returns(uint256) {
+        return 34;
+    }
+    
+}
